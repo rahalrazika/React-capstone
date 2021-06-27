52dbaeb1c2a19846f5d1eeaeb7bd68423a78cc66
rahalrazika <rahalraika@gmail.com>
2021-06-27 12:48:47 +0200

    add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import '../style/App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './Navbar';
+import ProductsList from './ProductsList';
+import DetailPage from './DetailPage';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Router>
+        <Switch>
+          <NavBar />
+          <Route path="/" exact component={ProductsList} />
+          <Route path="/product/id" exact component={DetailPage} />
+        </Switch>
+
+      </Router>
     </div>
   );
 }
