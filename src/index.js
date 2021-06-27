5f4c5c5f2db14463cd2a68c752d1fbfd7c39ed42
rahalrazika <rahalraika@gmail.com>
2021-06-27 11:08:25 +0200

    add store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './reducers/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
